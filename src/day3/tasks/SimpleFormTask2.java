03e48ef5f548f430afe085258d3bb0ae52a074c2
haceryapiciusa <56479407+haceryapiciusa@users.noreply.github.com>
2019-12-17 20:56:49 -0500

    puffff

--- src/day3/tasks/SimpleFormTask2.java
+++ src/day3/tasks/SimpleFormTask2.java
@@ -9,8 +9,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.List;
 public class SimpleFormTask2 {
-    public static void main(String[] args) {
-        System.setProperty( "webdriver.chrome.driver", "D:\\TechnoStudy\\Selenium\\ChromeDriver\\chromedriver.exe" );
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty( "webdriver.chrome.driver", "/Users/haceryapici/Desktop/Selenyum/chromedriver" );
         WebDriver driver = new ChromeDriver();
         driver.get( "https://www.seleniumeasy.com/test/basic-first-form-demo.html" );
         Integer number1 = 100;
@@ -38,7 +38,7 @@ public class SimpleFormTask2 {
         } else {
             System.out.println( "Failure!" );
         }
-
+        Thread.sleep(5000);
         driver.quit();
     }
 }
---
+++ src/day4/ChromeBrowser.java
@@ -0,0 +1,16 @@
+package day4;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class ChromeBrowser {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty( "webdriver.chrome.driver", "/Users/haceryapici/Desktop/Selenyum/chromedriver" );
+        WebDriver driver = new ChromeDriver();
+        driver.quit();
+    }
+}
